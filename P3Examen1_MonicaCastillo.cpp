17a3f4513ee0760d4f16b542e93f048a1b882906
monicastle <monicastillo@unitec.edu>
2020-08-21 15:10:36 -0600

    Haciendo el metodo Ping

--- P3Examen1_MonicaCastillo.cpp
+++ P3Examen1_MonicaCastillo.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include "PC.hpp"
 using namespace std;
 int main(){
     char respuesta = 's';
+    vector<PC *> lista_PCs;
     while (respuesta == 's'){
         int opcion;
         cout << "*******REDES*******" << endl;
@@ -21,17 +24,44 @@ int main(){
             cin >> mascara_red;
             cout << "Ingrese un Hostname: ";
             cin >> hostname;
-            
+            lista_PCs.push_back(new PC(IP, mascara_red, hostname));
+            cout << "Computadora agregada con exito!" << endl;
             break; }
         case 2: {
-
+            cout << "*********PCs*********" << endl;
+            for (int i = 0; i < lista_PCs.size(); i++) {
+                cout << i << ". " << lista_PCs[i]->toString() << endl;
+            } // Fin For
+            cout << "*********************" << endl;
+            int pos_PC;
+            cout << "Ingrese la posicion de la PC con la que desea comunicarse: ";
+            cin >> pos_PC;
+            while (pos_PC < 0 || pos_PC >= lista_PCs.size()) {
+                cout << "Posicion no valida!" << endl;
+                cout << "Ingrese la posicion de la PC con la que desea comunicarse: ";
+                cin >> pos_PC;
+            } // Fin While Validacion
+            PC pc;
+            string comando;
+            while (comando != "exit") {
+                cout << lista_PCs[pos_PC]->GetHostname() << "#";
+                cin >> comando;
+                if (comando == "show") {
+                    cout << "    IP: " << lista_PCs[pos_PC]->GetIP() << endl;
+                    cout << "    Netmask: " << lista_PCs[pos_PC]->GetMascaraRed() << endl;
+                } else if (comando == "exit"){
+                    return 0;
+                } else {
+                    pc.Ping(lista_PCs[pos_PC]->GetIP());
+                } // Fin If
+            } // Fin While Comandos
             break; }
         case 3: {
             // Aqui va la Salida
             respuesta = 'n';
             break; }
-        default: 
-            cout << "¡Entrada no valida!";        
+        default:
+            cout << "¡Entrada no valida!";
         } // Fin Switch
     } // Fin While Respuesta
 } // Fin Main
--- PC.hpp
+++ PC.hpp
@@ -1,16 +1,89 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cmath>
 using namespace std;
 class PC{
     string IP, mascara_red, hostname;
 public:
     PC(){      
     } // Fin Constructor PC
+
     PC(string IP, string mascara_red, string hostname){
         this->IP = IP;
         this->mascara_red = mascara_red;
         this->hostname = hostname;
     } // Fin Constructor PC
+
+    string GetIP(){
+        return IP;
+    } // Fin Get IP
+
+    void SetIP(string IP){
+        this->IP = IP;
+    } // Fin Set IP
+
+    string GetMascaraRed(){
+        return mascara_red;
+    } // Fin Get Mascara Red
+
+    void SetMascaraRed(string mascara_red){
+        this->mascara_red = mascara_red;
+    } // Fin Set Mascara Red
+
+    string GetHostname(){
+        return hostname;
+    } // Fin Get Mascara Red
+
+    void SetHostname(string hostname){
+        this->hostname = hostname;
+    } // Fin Set Mascara Red
+
+    void Ping(string IP){
+        vector<string> numeros_IP;
+        int cont = 0, numID;
+        string acum = "";
+        for (int i = 0; i < IP.size(); i++){
+            if (IP[i] == '.'){
+                numeros_IP.push_back(acum);
+                acum = "";
+            } else {
+                acum += IP[i];
+                if (i == IP.size() - 1){
+                    numeros_IP.push_back(acum);
+                    acum = "";                    
+                } // Fin If             
+            } // Fin If     
+        } // Fin For
+               cout << "lolis" << endl;
+        string IP_Binaria = "";
+        int acum1 = 0, acum2 = 0, digito, residuo;
+        for (int i = 0; i < 4; i++){
+            numID = stoi(numeros_IP[i]);
+            acum1 = 0;
+            acum2 = 0;
+            while(numID != 0){
+                digito = numID % 10;
+                if (digito == 0 || digito == 1) {
+                    int exp = 0, p = 1;
+                    p = p + digito * (int) pow(2, exp);
+                    exp++;
+                    residuo = numID % 10;
+                    numID = numID / 10;
+                    acum1 = acum1 + p;
+                    acum2 --;
+                } // Fin If
+            } // Fin While 
+            IP_Binaria += acum2 + ".";          
+        } // Fin For
+        cout << IP_Binaria << "IPDOPE";
+    } // Fin Metodo Ping
+
+    string toString(){
+        return IP + " | " + mascara_red + " | " + hostname;
+    } // Fin To String
+
     ~PC(){
     } // Fin Destructor PC
+
 }; // Fin Class PC
